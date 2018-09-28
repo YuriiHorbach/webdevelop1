cb4e80ebad9175f81bf0d937b41f8f35895f773c
southl <gyrii84@gmail.com>
2018-09-28 17:20:04 +0300

    add tab switcher

--- js/main.js
+++ js/main.js
@@ -13,5 +13,13 @@ $(document).ready(function(){
         $('.tab').not(tab).css({'display':'none'});
         $(tab).fadeIn(400);
     });
+
+
+		$(".tab_item").not(":first").hide();
+		$(".wrapper .tab").click(function() {
+			$(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+			$(".tab_item").hide().eq($(this).index()).fadeIn()
+		}).eq(0).addClass("active");
+
 				
 });
